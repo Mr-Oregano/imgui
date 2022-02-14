bac5f4127ea5bd556e371a41e3976b7420aed47e
Spice <kf.shared.ptr@gmail.com>
2022-02-13 22:29:34 -0600

    Update premake.

--- premake5.lua
+++ premake5.lua
@@ -5,18 +5,9 @@ project "ImGui"
 	targetdir "bin/%{cfg.buildcfg}_%{cfg.architecture}"
 	objdir "bin-int/%{cfg.buildcfg}_%{cfg.architecture}"
 
-	files
-	{
-        "imconfig.h",
-        "imgui.h",
-        "imgui.cpp",
-        "imgui_draw.cpp",
-        "imgui_internal.h",
-        "imgui_widgets.cpp",
-        "imstb_rectpack.h",
-        "imstb_textedit.h",
-        "imstb_truetype.h",
-        "imgui_demo.cpp"
+	files {
+        "./*.cpp",
+        "./*.h"
     }
     
 	filter "system:windows"
